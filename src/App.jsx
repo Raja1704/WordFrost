0756a401b00f8df127199a4a4f5de9eb2063b876
Raja Reddy <rajaonlyat1704@gmail.com>
2023-12-17 11:11:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import TypeBox from "./components/TypeBox"
+
+
+function App() {
+  return (
+    <div>
+         <Header />
+         <TypeBox />
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css"
+
+
+const Header = () => {
+
+
+    return (
+        <div id = "header">
+                 <h1 id = "MainTitle" >
+                         SpeedCount
+                  </h1>
+        </div>
+    )
+
+
+}
+
+export default Header
